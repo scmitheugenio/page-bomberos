9f998ff58dcadc4b3d5cad664f0f4a7ef6327591
eugenioscmith <129123097+scmitheugenio@users.noreply.github.com>
2023-12-21 21:54:50 -0300

    first commit

---
+++ index.html/script.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Función para mostrar un mensaje de bienvenida al cargar la página
+function mostrarMensajeBienvenida() {
+    alert("Bienvenido a la página de Bomberos Voluntarios de Oro Verde. Estamos aquí para ayudar.");
+}
+
+// Llama a la función cuando se carga la página
+document.addEventListener('DOMContentLoaded', function() {
+    mostrarMensajeBienvenida();
+    // Puedes agregar más funciones o código aquí según sea necesario
+});
